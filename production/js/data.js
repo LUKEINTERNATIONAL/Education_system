4756d6848e957c6b9c2d7a04d8c1b1410175d304
petroskayange <kayangepetros@gmail.com>
2023-06-26 15:38:08 +0200

    removing secondary level folder

---
+++ production/js/data.js
@@ -0,0 +1,49 @@
+var student_primary_data = [
+    [
+        '<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Catherine Banda',
+        '34501',
+        'STD 1',
+        'F',
+        '',
+        'No',
+        'No',
+        '<a href="#" class="btn btn-primary btn-xs" data-toggle="modal" data-target="#profileModal"><i class="fa fa-folder"></i> View </a>'
+    ],
+    [
+        'ppp',
+        'kkkk',
+        'ppp',
+        'kkkk',
+        'ppp',
+        'kkkk',
+        'ppp',
+        'kkkk',
+        'ppp'
+    ]
+]
+
+var student_secondary_data = [
+    [
+        '<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Catherine Banda',
+        '34501',
+        'Form 1',
+        'F',
+        '',
+        'No',
+        'No',
+        '<a href="#" class="btn btn-primary btn-xs" data-toggle="modal" data-target="#profileModal"><i class="fa fa-folder"></i> View </a>'
+    ],
+    [
+        'ppp',
+        'kkkk',
+        'ppp',
+        'kkkk',
+        'ppp',
+        'kkkk',
+        'ppp',
+        'kkkk',
+        'ppp'
+    ]
+]
