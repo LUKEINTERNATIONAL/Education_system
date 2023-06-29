c2e757a9b47b15b701a82674e9d22f8679010275
Josh-Reen <jgondwe59@gmail.com>
2023-06-29 12:04:21 +0200

    Avarta color changes for teachers

---
+++ production/data/teacher_attendance_data.js
@@ -0,0 +1,559 @@
+var student_primary_data = [
+    [
+        '<div class="avatar"><span>SC</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Japhet Nyirongo',
+        '45636',
+        'STD1',
+        'M',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`
+        
+    ],
+    [
+        '<div class="avatar"><span>CB</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Catherine Banda',
+        '34501',
+        'STD2',
+        'F',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`
+        
+    ],
+    [   '<div class="avatar"><span>DJ</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Daniel jere',
+        '35703',
+        'STD2',
+        'M',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`
+    ],
+    [   '<div class="avatar"><span>NK</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Natasha Kandulu',
+        '33554',
+        'STD5',
+        'F',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`
+    ],
+    [   '<div class="avatar"><span>DK</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Dominic Kasanga',
+        '35443',
+        'STD4',
+        'M',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`
+               
+        
+    ],
+    [   '<div class="avatar"><span>JB</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'John Biliati',
+        '34555',
+        'STD6',
+        'M',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`       
+        
+        
+    ],
+    [   '<div class="avatar"><span>MS</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Mary Simbazako',
+        '67891',
+        'STD7',
+        'F',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`       
+        
+        
+    ],
+    [   '<div class="avatar"><span>JK</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Joseph Kalibu',
+        '35535',
+        'STD8',
+        'M',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`       
+        
+        
+    ],
+    [   '<div class="avatar"><span>AC</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Ammon Chirwa',
+        '45357',
+        'STD7',
+        'M',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`       
+        
+        
+    ],
+    [   '<div class="avatar"><span>HG</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Hilda Gama',
+        '49488',
+        'STD1',
+        'F',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`       
+        
+        
+    ],
+    [   '<div class="avatar"><span>FM</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Frank Mandota',
+        '55464',
+        'STD3',
+        'M',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`       
+        
+        
+    ],
+    [   '<div class="avatar"><span>EL</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Edina Lajabu',
+        '54547',
+        'STD6',
+        'F',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`       
+        
+        
+    ],
+    [   '<div class="avatar"><span>JP</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Jane Phiri',
+        '75849',
+        'STD8',
+        'F',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`       
+        
+        
+    ],
+    [   '<div class="avatar"><span>PM</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Petros Mtandile',
+        '35667',
+        'STD2',
+        'M',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`       
+        
+        
+    ],
+    [   '<div class="avatar"><span>PB</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Patson Baula',
+        '74488',
+        'STD1',
+        'F',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`       
+        
+        
+    ],
+    [   '<div class="avatar"><span>BS</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Benard Singoyi',
+        '66744',
+        'STD3',
+        'M',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`       
+        
+        
+    ],
+    [   '<div class="avatar"><span>MM</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Memory Mabedi',
+        '54563',
+        'STD8',
+        'F',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`       
+        
+        
+    ],
+    [   '<div class="avatar"><span>JJ</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Jonathan Jembe',
+        '75899',
+        'STD6',
+        'M',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`       
+        
+        
+    ],
+    [   '<div class="avatar"><span>SK</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Samuel Kalua',
+        '52263',
+        'STD8',
+        'F',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`       
+        
+        
+    ],
+    [   '<div class="avatar"><span>VP</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Violet Phiri',
+        '85569',
+        'STD4',
+        'M',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`       
+        
+        
+    ]
+]
+
+var student_secondary_data = [
+    [
+        '<div class="avatar"><span>CB</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Shammah Chigwazi',
+        '45636',
+        'Form1',
+        'M',
+        `<div class="checkbox">
+												  <label>
+													<input type="checkbox" class="flat"> 
+												  </label>
+												</div>`
+        
+    ],
+    [
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        '<div class="avatar"><span>CB</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Catherine Banda',
+        '34501',
+        'Form2',
+        'F',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`
+        
+    ],
+    [   '<div class="avatar"><span>WM</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Wonderful Masano',
+        '35703',
+        'Form2',
+        'M',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`
+    ],
+    [   '<div class="avatar"><span>NK</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Natasha Kandulu',
+        '33554',
+        'Form3',
+        'F',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`,
+    ],
+    [   '<div class="avatar"><span>DK</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Dominic Kasanga',
+        '35443',
+        'Form4',
+        'M',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`
+               
+        
+    ],
+    [   '<div class="avatar"><span>PG</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Pacharo Gondwe',
+        '34555',
+        'Form2',
+        'M',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`       
+        
+        
+    ],
+    [   '<div class="avatar"><span>MS</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Mary Simbazako',
+        '67891',
+        'Form4',
+        'F',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`       
+        
+        
+    ],
+    [   '<div class="avatar"><span>CK</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Charlie Kadwa',
+        '35535',
+        'Form1',
+        'M',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`       
+        
+        
+    ],
+    [   '<div class="avatar"><span>AC</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Andrew Chidikho',
+        '45357',
+        'Form3',
+        'M',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`       
+        
+        
+    ],
+    [   '<div class="avatar"><span>GM</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Glory Mwafu',
+        '49488',
+        'Form1',
+        'F',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`       
+        
+        
+    ],
+    [   '<div class="avatar"><span>FM</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Frank Mandota',
+        '55464',
+        'Form3',
+        'M',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`       
+        
+        
+    ],
+    [   '<div class="avatar"><span>EL</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Edina Lajabu',
+        '54547',
+        'Form4',
+        'F',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`       
+        
+        
+    ],
+    [   '<div class="avatar"><span>KP</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Kumbukani Phiri',
+        '75849',
+        'Form2',
+        'F',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`       
+        
+        
+    ],
+    [   '<div class="avatar"><span>PK</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Petros Kayange',
+        '35667',
+        'Form1',
+        'M',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`       
+        
+        
+    ],
+    [   '<div class="avatar"><span>PB</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Patuma Bwanali',
+        '74488',
+        'Form3',
+        'F',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`       
+        
+        
+    ],
+    [   '<div class="avatar"><span>BC</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Benard Chinga',
+        '66744',
+        'Form4',
+        'M',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`       
+        
+        
+    ],
+    [   '<div class="avatar"><span>SS</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Suzan Shaba',
+        '54563',
+        'Form1',
+        'F',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`       
+        
+        
+    ],
+    [   '<div class="avatar"><span>JM</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Jonathan Mthunzi',
+        '75899',
+        'Form2',
+        'M',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`       
+        
+        
+    ],
+    [   '<div class="avatar"><span>EN</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Edward Nyirenda',
+        '52263',
+        'Form4',
+        'F',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`       
+        
+        
+    ],
+    [   '<div class="avatar"><span>KD</span></div>',
+        //'<img src="/production/images/user.png" class="rounded-circle img-fluid border border-white border-4" style="width: 50px;" alt="" width="50">',
+        'Kelvin Daniels',
+        '85569',
+        'Form1',
+        'M',
+        `<div class="checkbox">
+            <label>
+            <input type="checkbox" class="flat"> 
+            </label>
+        </div>`       
+        
+        
+    ]
+
+]
